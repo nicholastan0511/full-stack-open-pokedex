91ce8de029a9f00f4c15a7dff809cf730847af2a
Nicholas Tan <swaggychoz@gmail.com>
2024-06-25 18:53:43 +0800

    fix linting errors

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const path = require('path');
+const path = require('path')
 
 // get the port from env variable
 const PORT = process.env.PORT || 5000
@@ -9,11 +9,10 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+})
 
 app.listen(PORT, () => {
-
   // eslint-disable-next-line
   console.log(`server started on port ${PORT}`)
 })
