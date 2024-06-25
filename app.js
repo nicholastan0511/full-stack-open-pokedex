ae08eee43975e4c31a63f923e6b3496dfa828f89
Nicholas Tan <swaggychoz@gmail.com>
2024-06-25 18:50:20 +0800

    commit exercises 11.2~11.9

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const path = require('path');
 
 // get the port from env variable
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 app.listen(PORT, () => {
 
