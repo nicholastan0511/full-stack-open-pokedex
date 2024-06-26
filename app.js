2b7c8b7f4f243e75136de0986155259e7b29a98b
Nicholas Tan <swaggychoz@gmail.com>
2024-06-26 22:35:03 +0800

    add health path

--- app.js
+++ app.js
@@ -17,6 +17,10 @@ app.get('/version', (req, res) => {
   res.send('1')
 })
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line
   console.log(`server started on port ${PORT}`)
