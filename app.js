d8a624f3029ef780cc0db6587742ef25f0ca3e5a
Nicholas Tan <swaggychoz@gmail.com>
2024-06-27 14:00:38 +0800

    change health path

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.get('/version', (req, res) => {
 app.get('/health', (req, res) => {
   // eslint-disable-next-line no-constant-condition
 
-  res.send('okee')
+  res.send('okie dokie')
 })
 
 app.listen(PORT, () => {
